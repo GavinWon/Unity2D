52db6973a828b55ff1b9cdfa768f907a8fe4e1bd
Gavin <gavinmwong@gmail.com>
2019-09-26 23:49:44 -0700

    Added Number Wizard Game

---
+++ Number Wizard Game/Assets/NumberWizard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int max = 1000;
+        int min = 1;
+
+        Debug.Log("Welcome to number wizard");
+        Debug.Log("Pick an number");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you cna pick is: " + min);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
