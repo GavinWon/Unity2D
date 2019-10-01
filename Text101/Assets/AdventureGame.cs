0bf413bc0a421590fee72e927e8868720b5438ef
Gavin <gavinmwong@gmail.com>
2019-09-30 23:55:09 -0700

    continued lesson with text101

---
+++ Text101/Assets/AdventureGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = ("I am added programmaticaly!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
