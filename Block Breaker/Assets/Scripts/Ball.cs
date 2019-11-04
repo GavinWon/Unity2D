36aa16e23c172d1061a688feb27b5de2d597bfa0
Gavin <gavinmwong@gmail.com>
2019-11-03 18:53:15 -0800

    finished up to lesson 58 - destroy game objects

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // config params
+    [SerializeField] Paddle paddle;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+
+    //state
+    Vector2 paddleToBallVector;
+    private bool hasStarted = false;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+        
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
